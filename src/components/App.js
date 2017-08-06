257eac2f00c630b3b6981d7650f54440caa09f43
jamesmanone <boostedx@me.com>
2017-08-05 22:03:01 -0400

    Dev environment set up

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Hi there! &#x1F600;</h1>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.dev.js
@@ -0,0 +1,37 @@
+// Mostly borrowed from https://github.com/coryhouse/pluralsight-redux-starter
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src')
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new HtmlWebpackPlugin({ template: 'src/index.html' })
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel-loader']},
+      {test: /(\.css)$/, loaders: ['style', 'css']},
+      // {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      // {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+      // {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      // {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+    ]
+  }
+};
